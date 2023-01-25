aadc0b7ff681f6c4a6a6e7d7622167ee1abe76a9
elodeab <47059818+elodeab@users.noreply.github.com>
2023-01-25 23:51:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,66 @@
-from fastapi import FastAPI, HTTPException
-from models import Todos, User, Status
-from typing import List
-from uuid import uuid4
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel, Field
+from uuid import UUID
+
+## DB 
+import models 
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
-user = User(
-    id=uuid4(),
-         first_name="John",
-         last_name="Doe",
-         email="john.doe@example.com"
-        )
+## DB 
+models.Base.metadata.create_all(bind=engine)
 
-db: List[Todos] = [
-    Todos(
-        id=uuid4(),
-        title="Homework",
-        text="Doing homework",
-        completed = Status.doing,
-        owner = user
-    )
-]
+def get_db():
+    
+    try:
+        db = SessionLocal()
+        yield db
+        
+    finally:
+        db.close()
+
+class Todo(BaseModel):
+    text:str = Field(min_length=3)
+    status:str = Field(min_length=1,max_length=100)
+    user:str = Field(min_length=1,max_length=100)
 
 @app.get("/")
-def root():
-    return {"Hello":"World"}
+def read_api(db:Session = Depends(get_db)):
+    return db.query(models.Todos).all()
 
-@app.get("/api/v1/todos")
-def get_all_todos():
-    return db
+@app.post("/")
+def create_todo(todo:Todo, db:Session = Depends(get_db)):
+
+    todo_model = models.Todos()
+    
+    todo_model.text = todo.title
+    todo_model.status = todo.author
+    todo_model.user = todo.description
+    
+    db.add(todo_model)
+    db.commit()
+    
+    return todo
+
+@app.put("/{todo_id}")
+def update_todo(todo_id:int, todo:Todo, db:Session=Depends(get_db)):
+    
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    
+    if todo_model is None:
+
+        raise HTTPException(
+        status_code = 404,
+        detail = f"ID {todo_id} does not exist.")
+        
+    todo_model.title = todo.title
+    todo_model.author = todo.author
+    todo_model.description = todo.description
+    todo_model.rating = todo.rating
+    
+    db.add(todo_model)
+    db.commit()
+    
+    return todo
